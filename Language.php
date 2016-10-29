07b0720e6d75b936af4c420bf48db481cead4cc5
mrmacsi <macitsimsek12@hotmail.com>
2016-10-29 15:43:22 +0300

    Get, set language, get translates, save default language

--- Language.php
+++ Language.php
@@ -4,9 +4,61 @@ namespace Main;
 
 
 class Language{
+    private $pathName           =   "lang/";
+    private $langName           =   "tr";
+    private $defaultLangFile    =   "default.json";
+
+    function __construct()
+    {
+        if (!file_exists($this->pathName.$this->defaultLangFile)){
+            $fp = fopen($this->pathName.$this->defaultLangFile, 'w');
+            $array =['defaultLang'=>$this->langName];
+            fwrite($fp, json_encode($array));
+            fclose($fp);
+        }else{
+            $default        = json_decode(file_get_contents($this->pathName.$this->defaultLangFile), true);
+            $this->langName = $default['defaultLang'];
+        }
+    }
+
     function createNewLanguage($langName,$pathName=null){
-        if(!$pathName) $pathName = "/lang";
-        if (!file_exists($pathName)) mkdir($pathName."/".$langName, 0777);
-        return $langName;
+        if(!$pathName) $pathName = $this->pathName;
+        if (!file_exists($pathName)) mkdir($pathName, 0777);
+        $langName = str_replace('.json','',$langName).'.json';
+        if (!file_exists($pathName.$langName)){
+         fopen($pathName.$langName, 'w');
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    function addItem($array,$langName,$pathName=null){
+        if(!$pathName) $pathName = $this->pathName;
+        $langName = str_replace('.json','',$langName).'.json';
+        $fp = fopen($pathName.$langName, 'w');
+        fwrite($fp, json_encode($array));
+        fclose($fp);
+        return true;
+    }
+
+    function getAllTranslates($langName=null,$pathName=null){
+        if (!file_exists($pathName.$langName)) $pathName = $this->pathName;
+        if (!$langName) $langName = str_replace('.json','',$this->langName).'.json';
+        else $langName = str_replace('.json','',$langName).'.json';
+        return json_decode(file_get_contents($pathName.$langName), true);
+    }
+
+    function setLanguage($langName){
+        $fp = fopen($this->pathName.$this->defaultLangFile, 'w');
+        $this->langName=$langName;
+        $array =['defaultLang'=>$this->langName];
+        fwrite($fp, json_encode($array));
+        fclose($fp);
+        return true;
+    }
+
+    function getLanguage(){
+        return $this->langName;
     }
 }
